c051bbf89a2c137b3ee0ce84420b2eb380247915
tohara220 <tohara.220@gmail.com>
2024-08-03 14:07:54 +0900

    tailwindcssの設定反映されていなかった点を修正

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,10 +3,21 @@ import TodoForm from "@/components/TodoForm";
 
 export default function Home() {
   return (
-    <div>
-      <h1>TODO List</h1>
-      <TodoForm />
-      <TodoList />
-    </div>
+    <>
+      <header>
+        <h1 className="text-4xl font-bold text-blue-600">TODO List</h1>
+      </header>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1>TODO List</h1>
+        <main>
+          <div>
+            <TodoForm />
+          </div>
+          <div>
+            <TodoList />
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
